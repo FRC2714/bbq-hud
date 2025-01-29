e9eb9b6097ef019ffd8fe5c03c4eed6325c7a55d
sohan-bhat <sohanrambhat@gmail.com>
2025-01-28 18:45:24 -0600

    Added React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ReefHUD from './components/ReefHUD'
+
+
+function App() {
+  return (
+    <>
+      <ReefHUD />
+    </>
+  );
+}
+export default App;
---
+++ src/components/ReefHUD.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+
+const ReefHUD = () => {
+  return (
+    <div>ReefHUD</div>
+  )
+};
+export default ReefHUD;
