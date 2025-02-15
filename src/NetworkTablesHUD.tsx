093959ef1bca814d24713da53416bb760d203ad4
sohan-bhat <sohanrambhat@gmail.com>
2025-02-15 13:28:29 -0600

    Add responsive styles for hexagon and HUD elements

--- src/NetworkTablesHUD.tsx
+++ src/NetworkTablesHUD.tsx
@@ -39,28 +39,28 @@ const ReefHUD: React.FC = () => {
         };
     }, []);
 
-    useEffect(() => {
-        if (matchTimer != null ) {
-            if (matchTimer >= 135) {
-                document.body.classList.add('autonMode');
-            }
-            else if (matchTimer < 135 && matchTimer > 30) {
-                document.body.classList.remove('autonMode');
-                document.body.classList.add('teleopNormal');
-            }
-            else if (matchTimer <= 30 && matchTimer > 15) {
-                document.body.classList.remove('teleopNormal');
-                document.body.classList.add('remainingTime-30s');
-            }
-            else if (matchTimer <= 15 ) {
-                document.body.classList.remove('remainingTime-30s');
-                document.body.classList.add('remainingTime-15s');
-            }
-            else if (matchTimer < 1) {
-                document.body.classList.remove('remainingTime-15s');
-            }
-        }
-    }, [matchTimer]);
+    // useEffect(() => {
+    //     if (matchTimer != null ) {
+    //         if (matchTimer >= 135) {
+    //             document.body.classList.add('autonMode');
+    //         }
+    //         else if (matchTimer < 135 && matchTimer > 30) {
+    //             document.body.classList.remove('autonMode');
+    //             document.body.classList.add('teleopNormal');
+    //         }
+    //         else if (matchTimer <= 30 && matchTimer > 15) {
+    //             document.body.classList.remove('teleopNormal');
+    //             document.body.classList.add('remainingTime-30s');
+    //         }
+    //         else if (matchTimer <= 15 ) {
+    //             document.body.classList.remove('remainingTime-30s');
+    //             document.body.classList.add('remainingTime-15s');
+    //         }
+    //         else if (matchTimer < 1) {
+    //             document.body.classList.remove('remainingTime-15s');
+    //         }
+    //     }
+    // }, [matchTimer]);
 
     return (
         <div>
