6cbe640b878022e1955ecc4dd0d965258b4400e8
sohan-bhat <sohanrambhat@gmail.com>
2025-03-15 18:49:25 -0500

    Added robot mode handling and updated CSS for remaining time indicators

--- src/NetworkTablesHUD.tsx
+++ src/NetworkTablesHUD.tsx
@@ -8,6 +8,7 @@ const ReefHUD: React.FC = () => {
     const [matchTimer, setMatchTimer] = useState<number | null>(null);
     const [connection, setConnection] = useState<string>("Unknown");
     const [dragonState, setDragonState] = useState<string | null>(null);
+    const [robotMode, setRobotMode] = useState<string | null>(null);
 
     useEffect(() => {
         // Initialize and connect to NetworkTables
@@ -21,6 +22,7 @@ const ReefHUD: React.FC = () => {
         const stalkNumberTopic = ntcore.createTopic<number>('/SmartDashboard/Reef Stalk Number', NetworkTablesTypeInfos.kDouble);
         const matchTimerTopic = ntcore.createTopic<number>('/SmartDashboard/Match Info/Match Time', NetworkTablesTypeInfos.kDouble);
         const dragonStateTopic = ntcore.createTopic<string>('/SmartDashboard/Dragon/State', NetworkTablesTypeInfos.kString);
+        const robotModeTopic = ntcore.createTopic<string>('/SmartDashboard/Robot/Status', NetworkTablesTypeInfos.kString);
 
         // Subscribe to stalk number updates
         const subscriptionId1 = stalkNumberTopic.subscribe((value) => {
@@ -40,11 +42,18 @@ const ReefHUD: React.FC = () => {
             setDragonState(value);
         });
 
+        // Subscribe to robot mode updates
+        const subscriptionId4 = robotModeTopic.subscribe((value) => {
+            console.log(`Robot Mode; ${value}`);
+            setRobotMode(value);
+        })
+
         // Cleanup on component unmount
         return () => {
             stalkNumberTopic.unsubscribe(subscriptionId1);
             matchTimerTopic.unsubscribe(subscriptionId2);
             dragonStateTopic.unsubscribe(subscriptionId3);
+            robotModeTopic.unsubscribe(subscriptionId4);
         };
     }, []);
 
@@ -57,23 +66,24 @@ const ReefHUD: React.FC = () => {
     useEffect(() => {
         if (matchTimer != null ) {
             if (matchTimer !== -1) {
-                if (matchTimer >= 135) {
+                if (robotMode === "Autonomous") {
                     document.body.classList.add('autonMode');
-                }
-                else if (matchTimer < 135 && matchTimer > 30) {
-                    document.body.classList.remove('autonMode');
-                    document.body.classList.add('teleopNormal');
-                }
-                else if (matchTimer <= 30 && matchTimer > 15) {
-                    document.body.classList.remove('teleopNormal');
-                    document.body.classList.add('remainingTime-30s');
-                }
-                else if (matchTimer <= 15 ) {
-                    document.body.classList.remove('remainingTime-30s');
-                    document.body.classList.add('remainingTime-15s');
-                }
-                else if (matchTimer < 1) {
-                    document.body.classList.remove('remainingTime-15s');
+                } else if (robotMode === "Teleoperated") {
+                    if (matchTimer < 135 && matchTimer > 30) {
+                        document.body.classList.remove('autonMode');
+                        document.body.classList.add('teleopNormal');
+                    }
+                    else if (matchTimer <= 30 && matchTimer > 15) {
+                        document.body.classList.remove('teleopNormal');
+                        document.body.classList.add('remainingTime-30s');
+                    }
+                    else if (matchTimer <= 15 ) {
+                        document.body.classList.remove('remainingTime-30s');
+                        document.body.classList.add('remainingTime-15s');
+                    }
+                    else if (matchTimer < 1) {
+                        document.body.classList.remove('remainingTime-15s');
+                    }
                 }
             } else {
                 document.body.classList.remove('autonMode', 'remainingTime-30s', 'remainingTime-15s')
